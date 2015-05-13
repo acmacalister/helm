4d6d0bf6685889aec7783406e64264f99728d34b
Austin <acmacalister@gmail.com>
2015-05-13 16:32:20 -0700

    status for response serializers

--- response_serializers.go
+++ response_serializers.go
@@ -7,22 +7,24 @@ import (
 	"net/http"
 )
 
-func RespondWithJSON(w http.ResponseWriter, v interface{}) {
+func RespondWithJSON(w http.ResponseWriter, v interface{}, status int) {
 	json, err := json.Marshal(v)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("failed to marshal to json: %s", err), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 	w.Write(json)
 }
 
-func RespondWithXML(w http.ResponseWriter, v interface{}) {
+func RespondWithXML(w http.ResponseWriter, v interface{}, status int) {
 	xml, err := xml.Marshal(v)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("failed to marshal to xml: %s", err), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
 	w.Write(xml)
 }
